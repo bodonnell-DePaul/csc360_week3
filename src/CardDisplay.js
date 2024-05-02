50ab5290ed8ae55c4c8c4ceb3792903fa88d5dfe
Brian O'Donnell <brian.m.odonnell@gmail.com>
2024-05-02 01:56:38 +0000

    front end, json joy

--- src/CardDisplay.js
+++ src/CardDisplay.js
@@ -20,7 +20,8 @@ import Alert from 'react-bootstrap/Alert';
 
 function CardDisplay(){
 
-    const [recipes, setRecipe ] = useState(api_recipes);
+    const [recipes, setRecipe ] = useState(null);
+    const [recipeIngredients, setIngredients ] = useState(null);
     const [trigger, setTrigger] = useState(0);
     const [prepared, setPrepared ] = useState(false);
     const [activeModal, setActiveModal] = useState(null);   
@@ -28,127 +29,165 @@ function CardDisplay(){
     const [apiWeather, setWeatherData] = useState(null); 
     
 
-    function showModal(modalId) {
+    function showModal(modalId) 
+    {
         setActiveModal(modalId);
-    };
+        if(recipes !== null)
+        {
+            //Iterate over the array of recipes
+            for(let i = 0; i < recipes.length; i++){
+                if(modalId.indexOf(i) > -1)
+                {
+                    console.log(modalId);
+                    fetch('http://127.0.0.1:5172/recipeIngredients/{repcipeTitle}?recipeTitle='+recipes[i].title)
+                    .then(response => response.json())
+                    .then(data => { 
+                        setIngredients(data);
+                        console.log(data);
+                    })  
+                }
+            }
+        }
+    }
 
+    useEffect(() => {
+
+        // Check each ingredient of the current recipe
+        if(recipeIngredients)
+        {
+            for(let i =0; i< recipeIngredients.length; i++)
+            {
+
+                let prepared = true;
+                if(recipeIngredients[i].prepared === false)
+                {
+                    prepared = false;
+                    setPrepared(prepared);
+                }
+            }
+        }
+
+    }, [recipeIngredients]);
+                    
     function hideModal(){
         setActiveModal(null);
     };
 
     function ingredientClick(title,index) {
-        for (let i = 0; i < recipes.length; i++) {
-            if(recipes[i].title === title){
-                for(let j = 0; j < recipes[i].ingredients.length; j++){
-                    if (j === index) {
-                        recipes[i].ingredients[index].prepared = !recipes[i].ingredients[index].prepared
-                        setRecipe(recipes);
-                        setTrigger(trigger + 1);
-                    }
-                }   
+        for (let i = 0; i < recipeIngredients.length; i++) {
+            if(recipeIngredients[i].recipeTitle === title){
+                recipeIngredients[index].prepared = !recipeIngredients[index].prepared
+                setIngredients(recipeIngredients);
+                //setTrigger(trigger + 1);
+
+                // for(let j = 0; j < recipeIngredients.ingredients.length; j++){
+                //     if (j === index) {
+                //         recipes[i].ingredients[index].prepared = !recipes[i].ingredients[index].prepared
+                //         setRecipe(recipes);
+                //         setTrigger(trigger + 1);
+                //     }
+                // }   
             }
         }
     }
 
+    
     // useEffect hook to perform side effects in the component
     useEffect(() => {
 
         if(apiHelloWorld === null){
-            fetch('http://127.0.0.1:5172/recipes')
-            .then(response => response.text())
-            .then(data => setHelloWorld(data))
+                fetch('http://127.0.0.1:5172/recipes')
+                .then(response => response.text())
+                .then(data => { 
+                    setHelloWorld(data);
+                    console.log(data);
+            })
+            console.log(apiHelloWorld);
         }
 
         if(apiWeather === null){
             fetch('http://127.0.0.1:5172/weatherforecast')
             .then(response => response.json())
-            .then(data => setWeatherData(data))
+            .then(data => {
+                setWeatherData(data)
+                console.log(data);
+            })
+            console.log(apiWeather)
         }
 
-        fetch('http://127.0.0.1:5172/recipeTitles')
-        .then(response => response.json())
-        .then(data => setRecipe(data))
-
-
-        console.log(recipes);
-        // Iterate over the array of recipes
-        for(let i = 0; i < recipes.length; i++){
-            // Assume the recipe is prepared initially
-            let prepared = true;
-
-            // Check each ingredient of the current recipe
-            if(recipes[i].ingredients){
-                for(let j = 0; j < recipes[i].ingredients.length; j++){
-                    // If any ingredient is not prepared, set the prepared flag to false
-                    if(recipes[i].ingredients[j].prepared === false){
-                        prepared = false;
-                        // Update the prepared state to false
-                        setPrepared(prepared);
-                        break;
-                        
-                    }
-                }
-            }
-
-            // If all ingredients are prepared, update the prepared state to true
-            if(prepared){
-                setPrepared(prepared);
-                break;
-            }
+        if(recipes === null){
+            fetch('http://127.0.0.1:5172/recipeTitles')
+            .then(response => response.json())
+            .then(data => {
+                    setRecipe(data);
+                    console.log(data);
+                })
         }
+         console.log(recipes);
+
+
+        // If all ingredients are prepared, update the prepared state to true
+        if(prepared){
+            setPrepared(prepared);
+            //break;
+        }
+        
 
         // Update the recipe state with the current recipes array
-        setRecipe(recipes);
+        //setRecipe(recipes);
     // The effect will re-run when 'trigger' or 'recipes' change
     }, [trigger, recipes]);
 
     
 
-    const cards = api_recipes.map((recipe, index) => { 
-        return (
-            <Col key={index}>
-                <Card style={{ width: '20rem' }}>
-                    <Card.Img variant="top" src={recipe.imageUrl} />
-                    <Card.Body>
-                        <Card.Title>
-                            <RecipeTitle title={recipe.title} review={recipe.review}/> 
-                        </Card.Title>
-                        <Card.Text>
-                            {recipe.description}
-                        </Card.Text>
-                        <Button variant="primary" onClick={() => showModal(`${index}myModal`)}>Show Details</Button>
-                    </Card.Body>
-                </Card>
+    if (recipes !== null){
+        const cards = recipes.map((recipe, index) => { 
+            console.log(recipe);
+            return (
+                <Col key={index}>
+                    <Card style={{ width: '20rem' }}>
+                        <Card.Img variant="top" src={recipe.imageUrl} />
+                        <Card.Body>
+                            <Card.Title>
+                                <RecipeTitle title={recipe.title} review={recipe.review}/> 
+                            </Card.Title>
+                            <Card.Text>
+                                {recipe.description}
+                            </Card.Text>
+                            <Button variant="primary" onClick={() => showModal(`${index}myModal`)}>Show Details</Button>
+                        </Card.Body>
+                    </Card>
 
-                <Modal id={`${index}myModal`} show={activeModal === `${index}myModal`} onHide={hideModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{recipe.title}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <IngredientList ingredients={recipe.ingredients} title={recipe.title} onClick={ingredientClick}/>
-                        { prepared ? <Alert variant='success'><Alert.Heading>Prep work done!</Alert.Heading></Alert> : <Alert variant='danger'><Alert.Heading>Just keep chopping</Alert.Heading></Alert>}
-                        <PreperationSteps steps={recipe.steps}/>
-                    </Modal.Body>
-                </Modal>
-            </Col>
-        );
-    });
-    return(
-        <Container>
-            <h1>{apiHelloWorld}</h1>
-            <Row>
-                {cards}
-            </Row>
-            {apiWeather && apiWeather.map((day,index) => (
-                    <div key={index}>
-                        <h1>Date: {day.date}</h1>
-                        <h3>Temp: {day.temperatureF}</h3>
-                        <h3>Summary: {day.summary}</h3>
-                    </div>
-                ))
-            }
-        </Container>
-    )
+                    <Modal id={`${index}myModal`} show={activeModal === `${index}myModal`} onHide={hideModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{recipe.title}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <IngredientList ingredients={recipeIngredients} title={recipe.title} onClick={ingredientClick}/>
+                            {/* { prepared ? <Alert variant='success'><Alert.Heading>Prep work done!</Alert.Heading></Alert> : <Alert variant='danger'><Alert.Heading>Just keep chopping</Alert.Heading></Alert>}
+                            <PreperationSteps steps={recipe.steps}/> */}
+                        </Modal.Body>
+                    </Modal>
+                </Col>
+            );
+        });
+        return(
+            <Container>
+                <h1>{apiHelloWorld}</h1>
+                <Row>
+                    {cards}
+                </Row>
+                {apiWeather && apiWeather.map((day,index) => (
+                        <div key={index}>
+                            <h1>Date: {day.date}</h1>
+                            <h3>Temp: {day.temperatureF}</h3>
+                            <h3>Summary: {day.summary}</h3>
+                        </div>
+                    ))
+                }
+            </Container>
+        )
+    }
 }
 
 export default CardDisplay;
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -4,21 +4,24 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 function IngredientList(props) {
 
-    // Create the list items using map
-    const ingredientListItems = props.ingredients.map((ingredient, index) => {
-        return (  
-            
-            <ListGroup.Item as="li" key={index} className={ ingredient.prepared ? 'prepared' : '' }  onClick={ () => props.onClick(props.title,index) }>
-                {ingredient.name}
-            </ListGroup.Item>   
+    if(props.ingredients !== null)
+    {
+        // Create the list items using map
+        const ingredientListItems = props.ingredients.map((ingredient, index) => {
+            return (  
+                
+                <ListGroup.Item as="li" key={index} className={ ingredient.prepared ? 'prepared' : '' }  onClick={ () => props.onClick(props.title,index) }>
+                    {ingredient.name}
+                </ListGroup.Item>   
+            );
+        });
+        // return the HTML for the component
+        // ingredientListItems will be rendered as li elements
+        return (
+            <ListGroup as="ul">
+                { ingredientListItems }
+            </ListGroup>
         );
-    });
-    // return the HTML for the component
-    // ingredientListItems will be rendered as li elements
-    return (
-        <ListGroup as="ul">
-            { ingredientListItems }
-        </ListGroup>
-    );
+    }
 }
 export default IngredientList;
